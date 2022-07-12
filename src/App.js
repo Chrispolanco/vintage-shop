e2d3dbf499ba2a2417d721901e847c9355776ae0
Chrispolanco <chrispolanco@sbcglobal.net>
2022-07-12 16:02:45 -0700

    added sass

--- src/App.js
+++ src/App.js
@@ -29,6 +29,7 @@ const App = () => {
     <div className="categories-container">
       {categories.map(({title}) => (
         <div className="category-container">
+          <div className="background-image" />
           <div className="category-body-container">
             <h2> {title} </h2>
             <p> Shop Now </p>
