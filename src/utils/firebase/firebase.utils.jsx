ccbfbc641576585becfb4f183b7911109d754dbe
Chrispolanco <chrispolanco@sbcglobal.net>
2022-07-17 15:19:05 -0700

    created ability to add users to firebase db

--- src/utils/firebase/firebase.utils.jsx
+++ src/utils/firebase/firebase.utils.jsx
@@ -37,8 +37,22 @@ import {
 
   export const createUserDocumentFromAuth = async (userAuth) => {
     const userDocRef = doc(db, 'users', userAuth.uid); 
-
-
     const userSnapShot = await getDoc(userDocRef); 
+
+    if(!userSnapShot.exists()){
+        const { displayName, email } = userAuth; 
+        const createdAt = new Date(); 
+
+        try{
+            await setDoc(userDocRef, {
+                displayName, 
+                email, 
+                createdAt 
+            }); 
+        } catch (error) {
+            console.log('error creating the user', error.message); 
+        }
+    }
+        return userDocRef; 
   }
 
