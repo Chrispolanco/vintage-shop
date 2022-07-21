65e56562e10e4446979b82c1757dc44304a4f89d
Chrispolanco <chrispolanco@sbcglobal.net>
2022-07-21 10:27:55 -0700

    refracting authentication and signin

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -28,9 +28,20 @@ const SignInForm = () => {
         event.preventDefault(); 
 
         try{    
-            const response = await signInAuthUserWithEmailAndPassword(email, password); 
+            await signInAuthUserWithEmailAndPassword(email, password); 
             resetFormFields(); 
-        }catch(error){}
+        }catch(error){
+            switch(error.code){
+                case 'auth/wrong-password': 
+                    alert('Incorrect password for email');
+                    break; 
+                case 'auth/user-not-found': 
+                    alert('No user associated with this email');
+                    break; 
+                default:
+                    console.log(error); 
+            }
+        }
     }
 
     const handleChange = (event) => {
@@ -48,7 +59,7 @@ const SignInForm = () => {
                 <FormInput label="Password" type="password" required onChange={handleChange} name="password" value={password}/> 
                 <div className="buttons-container">
                     <Button type="submit">Sign In</Button>
-                    <Button buttonType='google' onClick={signInWithGoogle}>Google Sign In</Button>
+                    <Button type='button' buttonType='google' onClick={signInWithGoogle}>Google Sign In</Button>
                 </div>
             </form>
         </div>
--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,13 +1,12 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth} from '../../utils/firebase/firebase.utils.jsx'
-
 import SignUpForm from '../../components/sign-up-form/sign-up-form.component.jsx';
 import SignInForm from '../../components/sign-in-form/sign-in-form.component.jsx';
+
+import './authentication.styles.scss'; 
 
 const Authentication = () => {
 
     return(
-        <div>
-            <h1> Sign In Page </h1>
+        <div className='authentication-container'>
             <SignInForm/> 
             <SignUpForm/>
         </div>
