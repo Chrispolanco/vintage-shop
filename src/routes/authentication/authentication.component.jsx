3917e4f14a0735e895d648bb3b93c16e84ceb855
Chrispolanco <chrispolanco@sbcglobal.net>
2022-07-29 15:46:00 -0700

    refractor and added useEffect to cart context

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -4,7 +4,6 @@ import SignInForm from '../../components/sign-in-form/sign-in-form.component.jsx
 import './authentication.styles.scss'; 
 
 const Authentication = () => {
-
     return(
         <div className='authentication-container'>
             <SignInForm/> 
