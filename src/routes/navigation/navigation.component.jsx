255463f0d3ff0c7b1ef3e7a669f7684a9d9d989b
Chrispolanco <chrispolanco@sbcglobal.net>
2022-07-28 19:52:30 -0700

    created cart doopdown and cart context

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,9 +1,12 @@
 import { Fragment, useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
 
-import { ReactComponent as HalloweenLogo } from '../../assets/halloween.svg'
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.components";
+
 import { UserContext } from "../../contexts/user.context";
 
+import { ReactComponent as HalloweenLogo } from '../../assets/halloween.svg'
 import {signOutUser} from '../../utils/firebase/firebase.utils'; 
 
 import './navigation.styles.scss'
@@ -26,7 +29,9 @@ const Navigation = () => {
           ) : (
             <Link className="nav-link" to='/auth'>Sign In</Link>
           )}
+          <CartIcon /> 
         </div>
+        <CartDropdown></CartDropdown>
       </div>
       <Outlet/> 
     </Fragment>
