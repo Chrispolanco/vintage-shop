5eb66ea3c513ced89e24ac53c91ef580bceb7e5f
Chrispolanco <chrispolanco@sbcglobal.net>
2022-07-30 23:44:20 -0700

    increment items checkout page/component

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,9 +1,30 @@
+import { useContext } from 'react';
+
+import { CartContext } from '../../contexts/cart.context';
+
 import './checkout.styles.scss'; 
 
 const Checkout = () => {
+    const { cartItems, addItemToCart } = useContext(CartContext); 
+    
     return (
         <div>
-            <h1>I'm here</h1>
+            <h1>Checkout Page</h1>
+            <div>
+                {
+                    cartItems.map((cartItem) => {
+                        const {id, name, quantity } = cartItem; 
+                        return(
+                            <div key={id}>
+                                <h2>{name}</h2>
+                                <span>{quantity}</span>
+                                <span> Decrement </span>
+                                <span onClick={() => addItemToCart(cartItem)}> Increment </span>
+                            </div>
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 }; 
