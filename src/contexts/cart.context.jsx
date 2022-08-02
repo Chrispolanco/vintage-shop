7acdfcb1f5982802fcc995dfc40d6448984674de
Chrispolanco <chrispolanco@sbcglobal.net>
2022-08-01 22:46:58 -0700

    remove item from checkout page

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -31,12 +31,17 @@ const removeCartItem = (cartItems, cartItemToRemove) => {
     ); 
 }; 
 
+const clearCartItem = (cartItems, cartItemToClear) => {
+    return cartItems.filter((cartItem) => cartItem.id !== cartItemToClear.id); 
+}; 
+
 export const CartContext = createContext({
     isCartOpen: false, 
     setIsCartOpen: () => {},
     cartItems: [], 
     addItemToCart: () => {},
     removeItemFromCart: () => {},
+    clearItemFromCart: () => {},
     cartCount: 0
 })
 
@@ -58,7 +63,19 @@ export const CartProvider = ({ children }) => {
         setCartItems(removeCartItem(cartItems, cartItemToRemove)); 
     }
 
-    const value = { isCartOpen, setIsCartOpen, addItemToCart, removeItemFromCart, cartItems, cartCount }; 
+    const clearItemFromCart = (cartItemToClear) => {
+        setCartItems(clearCartItem(cartItems, cartItemToClear)); 
+    }
+
+    const value = { 
+        isCartOpen, 
+        setIsCartOpen, 
+        addItemToCart, 
+        removeItemFromCart, 
+        cartItems, 
+        cartCount, 
+        clearItemFromCart 
+    }; 
 
     return(
         <CartContext.Provider value={value}> {children} </CartContext.Provider>
