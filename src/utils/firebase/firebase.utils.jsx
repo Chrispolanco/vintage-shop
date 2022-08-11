99e76dd7dfa60c8e7140ea312d1068de467c03e8
Chrispolanco <chrispolanco@sbcglobal.net>
2022-08-10 23:45:18 -0700

    implemented category map

--- src/utils/firebase/firebase.utils.jsx
+++ src/utils/firebase/firebase.utils.jsx
@@ -10,7 +10,16 @@ import {
   onAuthStateChanged
 } from 'firebase/auth'; 
 
-import {getFirestore, doc, getDoc, setDoc, collection, writeBatch} from 'firebase/firestore'; 
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+  collection,
+  writeBatch,
+  query, 
+  getDocs
+} from 'firebase/firestore'; 
 
 const firebaseConfig = {
   apiKey: "AIzaSyCOmDMLmsk7aNugJPYhoQ85xtAPkprgeY8",
@@ -48,6 +57,20 @@ export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => 
 
   await batch.commit(); 
   console.log('done'); 
+}; 
+
+export const getCategoriesAndDocuments = async () => {
+  const collectionRef = collection(db, 'categories'); 
+  const q = query(collectionRef); 
+
+  const querySnapshot = await getDocs(q); 
+  const categoryMap = querySnapshot.docs.reduce((acc, docSnapShot) => {
+    const {title, items} = docSnapShot.data(); 
+    acc[title.toLowerCase()] = items; 
+    return acc; 
+  }, {}); 
+
+  return categoryMap; 
 }; 
 
 export const createUserDocumentFromAuth = async (userAuth, additionalInformation = {}) => {
